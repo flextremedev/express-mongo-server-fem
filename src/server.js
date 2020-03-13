97106fb4780f387558191d5372a52d9d783dadd9
Pascal Wegner <pascal.wegner@fastleansmart.com>
2020-03-13 11:16:16 +0100

    Initial setup

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import { json, urlencoded } from 'body-parser'
+import morgan from 'morgan'
+import cors from 'cors'
+
+export const app = express()
+
+app.disable('x-powered-by')
+
+app.use(cors())
+app.use(json())
+app.use(urlencoded({ extended: true }))
+app.use(morgan('dev'))
+
+export const start = () => {
+  app.listen(3000, 'Server listening on 3000...')
+}
