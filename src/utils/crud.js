f109eb27b4011d9deb5d9feb0f80b8b4999b87c7
flextremedev <p.wegner1992@gmail.com>
2020-03-18 17:00:52 +0100

    updateOne controller creator

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -18,7 +18,17 @@ export const createOne = model => async (req, res) => {
   res.status(201).json({ data: doc })
 }
 
-export const updateOne = model => async (req, res) => {}
+export const updateOne = model => async (req, res) => {
+  const doc = await model.findOneAndUpdate(
+    { _id: req.params.id, createdBy: req.user._id },
+    req.body,
+    { new: true }
+  )
+  if (!doc) {
+    return res.status(400).end()
+  }
+  res.status(200).json({ data: doc })
+}
 
 export const removeOne = model => async (req, res) => {}
 
